9c8f3751924a8890359e520a2db4977673ee493e
Chan Kar Fung <ckf3090@gmail.com>
2025-05-09 18:42:31 +0800

    small changes

---
+++ Mini IT project/login.js
@@ -0,0 +1,31 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault(); 
+  
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+  
+    const usernameError = document.getElementById("username-error");
+    const passwordError = document.getElementById("password-error");
+  
+    let isValid = true;
+  
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+  
+    if (username.value.trim().length < 5) {
+      usernameError.textContent = "Username must be at least 5 characters.";
+      isValid = false;
+    }
+  
+    if (password.value.trim().length < 6) {
+      passwordError.textContent = "Password must be at least 6 characters.";
+      isValid = false;
+    }
+  
+    if (isValid) {
+
+      alert("Work in progress");
+    }
+  });
+  
+  
