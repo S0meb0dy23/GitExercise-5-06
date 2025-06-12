5fe2026057d23859045acca425637f5e516885dc
lirili larila <ivanchongkhaiwen@gmail.com>
2025-06-12 21:10:01 +0800

    link fix

--- pet_community/db.php
+++ pet_community/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "pet_community");
+$conn = new mysqli("localhost", "root", "", "server_db");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
