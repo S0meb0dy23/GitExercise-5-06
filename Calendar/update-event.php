b69b4ec91ccb397c922e821f453393baa1e3616d
Bonky321 <preshen2302@gmail.com>
2025-06-13 03:18:00 +0800

    event

--- Calendar/update-event.php
+++ Calendar/update-event.php
@@ -1,14 +1,21 @@
 <?php
+session_start();
 include 'db.php';
+
+// Check if user is logged in
+if (!isset($_SESSION['username'])) {
+    die("Unauthorized");
+}
 
 $id = $_POST['id'];
 $title = $_POST['title'];
 $type = $_POST['type'];
 $time_from = $_POST['time_from'];
 $time_to = $_POST['time_to'];
+$username = $_SESSION['username'];
 
-$stmt = $mysqli->prepare("UPDATE events SET title = ?, type = ?, time_from = ?, time_to = ? WHERE id = ?");
-$stmt->bind_param("ssssi", $title, $type, $time_from, $time_to, $id);
+$stmt = $mysqli->prepare("UPDATE events SET title = ?, type = ?, time_from = ?, time_to = ? WHERE id = ? AND username = ?");
+$stmt->bind_param("ssssis", $title, $type, $time_from, $time_to, $id, $username);
 
 if ($stmt->execute()) {
     echo "Event updated";
